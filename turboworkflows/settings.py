7e5ad7d3561d9bb2184d56bebdfe852fe36fabc4
ricardo-alternova <ricardo@alternova.dev>
2024-01-15 23:21:07 +0100

    add new apps to settings

--- turboworkflows/settings.py
+++ turboworkflows/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.defaults",
+    "apps.authentication"
 ]
 
 MIDDLEWARE = [
