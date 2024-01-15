e5de93a1f046acb7d0b027ddb34807119c168e1d
ricardo-alternova <ricardo@alternova.dev>
2024-01-15 22:06:25 +0100

    fix the import from the decouple library

--- turboworkflows/settings.py
+++ turboworkflows/settings.py
@@ -11,12 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import Config, Csv
+from decouple import config, Csv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-config = Config()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
