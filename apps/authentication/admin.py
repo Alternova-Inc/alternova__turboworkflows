e65c53fdec0d82e275ddc39bdf6afaaeefbb9b63
ricardo-alternova <ricardo@alternova.dev>
2024-01-19 19:19:50 +0100

    start using the base admin class

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -5,23 +5,43 @@ from apps.authentication.models.profile import Profile
 from apps.authentication.models.company import Company
 from apps.authentication.models.company_profile_set import CompanyProfileSet
 from apps.authentication.models.role import Role
+from apps.defaults.admin import BaseModelAdmin
 
 class CompanyProfileSetInline(admin.TabularInline):
     model = CompanyProfileSet
     extra = 0  # number of extra forms to display
 
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_name')
+class ProfileAdmin(BaseModelAdmin):
+    list_extend = ('user_name', 'user_email', 'user_username', 'user_is_staff', 'user_is_superuser', 'user_last_login',)
+    search_fields_extend = ('user__first_name', 'user__last_name', 'user__email', 'user__username',)
     inlines = [CompanyProfileSetInline]
 
     def user_name(self, obj):
         return f"{obj.user.first_name} {obj.user.last_name}"
+    
+    def user_email(self, obj):
+        return obj.user.email
 
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('id', 'company_name')
+    def user_username(self, obj):
+        return obj.user.username
 
-class RoleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'role_name', 'company')
+    def user_is_staff(self, obj):
+        return obj.user.is_staff
+
+    def user_is_superuser(self, obj):
+        return obj.user.is_superuser
+
+    def user_last_login(self, obj):
+        return obj.user.last_login
+
+class CompanyAdmin(BaseModelAdmin):
+    list_extend = ('company_name',)
+    search_fields_extend = ('company_name',)
+
+    
+class RoleAdmin(BaseModelAdmin):
+    list_extend = ('role_name', 'company',)
+    search_fields_extend = ('role_name', 'company__company_name',)
     filter_horizontal = ('workflows',)
 
 
--- apps/workflows/admin.py
+++ apps/workflows/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from apps.defaults.admin import BaseModelAdmin
 
 from apps.workflows.models.workflow import Workflow
 
 # Register your models here.
-class WorkflowAdmin(admin.ModelAdmin):
-    list_display = ('id', 'workflow_name')
+class WorkflowAdmin(BaseModelAdmin):
+    list_extend = ('workflow_name',)
+    search_fields_extend = ('workflow_name',)
 
 
 admin.site.register(Workflow, WorkflowAdmin)
