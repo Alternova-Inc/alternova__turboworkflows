8f7e9b5c7599896f32d1c1cae6065cfce36b9aae
ricardo-alternova <ricardo@alternova.dev>
2024-02-21 19:03:02 +0100

    add docstring

--- apps/workflows/signals.py
+++ apps/workflows/signals.py
@@ -5,6 +5,9 @@ from apps.workflows.models.workflow import Workflow
 
 @receiver(post_save, sender=Workflow)
 def add_workflow_to_role(sender, instance, created, **kwargs):
+    """
+    Add a workflow to the admin role's workflows if the instance is newly created.
+    """
     if created:
         # Get the admin role for the company
         admin_role = Role.objects.get(company=instance.company, role_name='admin')
