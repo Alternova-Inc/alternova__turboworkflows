07fd2ef08d4fa1bf8a884324b02c5994e487fedb
ricardo-alternova <ricardo@alternova.dev>
2024-02-21 17:34:58 +0100

    re-add removed validations (just the correct ones)

--- apps/workflows/models/workflow_sequence.py
+++ apps/workflows/models/workflow_sequence.py
@@ -36,6 +36,7 @@ class WorkflowSequence(BaseModel):
         return f'{self.workflow.workflow_name} - Sequence'
 
     def clean(self):
+        # Validate that at least one of form_step, approval_step, or action_step is selected
         if not self.form_step and not self.approval_step and not self.action_step:
             raise ValidationError("At least one of form_step, approval_step, or action_step must be selected.")
 
@@ -49,12 +50,9 @@ class WorkflowSequence(BaseModel):
             raise ValidationError("Only one of form_step, approval_step, or action_step can be filled.")
 
     def validate_company(self, *args, **kwargs):
-        from apps.authentication.models.company_profile_set import CompanyProfileSet
-        
-        current_user = kwargs['current_user']
         type = kwargs['type']
 
-        # first, validate that worflow and the selected step belong to the same company
+        # validate that worflow and the selected step belong to the same company
         if type == 'Form':
             if self.workflow.company != self.form_step.company:
                 raise ValidationError("Workflow and Form Step must belong to the same company.")
@@ -65,7 +63,7 @@ class WorkflowSequence(BaseModel):
             if self.workflow.company != self.action_step.company:
                 raise ValidationError("Workflow and Action Step must belong to the same company.")
 
-    def save(self, *args, **kwargs):            
+    def save(self, *args, **kwargs):           
         self.full_clean()
         
         # Always save the type automatically
@@ -76,5 +74,7 @@ class WorkflowSequence(BaseModel):
         elif self.action_step:
             self.type = 'Action'
 
+        # Validate that the workflow and the selected step belong to the same company
+        self.validate_company(type=self.type)
         super().save(*args, **kwargs)
         
