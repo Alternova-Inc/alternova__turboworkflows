4e9ac28e9f8da38c49830ef24cac11af13c35735
ricardo-alternova <ricardo@alternova.dev>
2024-02-21 17:19:41 +0100

    add docstring

--- apps/authentication/signals.py
+++ apps/authentication/signals.py
@@ -5,6 +5,9 @@ from apps.authentication.models.company import Company
 
 @receiver(post_save, sender=Company)
 def create_role_for_company(sender, instance, created, **kwargs):
+    """
+    - Create default roles for a company when it is created.
+    """
     if created:
         Role.objects.create(company=instance, role_name='default', is_default=True)
         Role.objects.create(company=instance, role_name='admin')
