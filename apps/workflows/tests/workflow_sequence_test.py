ca374fb06ef1c2807dba742fa4a8fcb551f136be
ricardo-alternova <ricardo@alternova.dev>
2024-02-21 17:52:32 +0100

    add docstring

--- apps/workflows/tests/workflow_sequence_test.py
+++ apps/workflows/tests/workflow_sequence_test.py
@@ -40,23 +40,31 @@ class WorkflowSequenceTest(TestCase):
         self.user_form_content2 = UserFormContent(user_form=self.user_form2, field=self.field2, order=1)
         self.user_form_content2.save(current_user=self.user2)
 
-    def test_validate_company(self):
+    def validate_type_assignation(self):
+        '''
+        Checks that the saved UserFormContent has the expected automatically assigned attributes
+        '''
         workflow_sequence = WorkflowSequence(workflow=self.workflow1, form_step=self.user_form1, order=1)
         workflow_sequence.save(current_user=self.user1)  # Pass the current user as an argument to the save() method
         saved_workflow_sequence = WorkflowSequence.objects.get(id=workflow_sequence.id)
 
-        # Check that the saved UserFormContent has the expected attributes
+        
         self.assertEqual(saved_workflow_sequence.type, 'Form')
 
+    def validate_single_step_condition(self):
+        '''
+        Checks that the workflow_sequence can't have more than one step
+        '''
         workflow_sequence = WorkflowSequence(workflow=self.workflow1, form_step=self.user_form1, approval_step=self.user_form3, order=1)
         with self.assertRaises(ValidationError):
             workflow_sequence.full_clean() # Should raise ValidationError because two steps were added
 
+    def test_validate_company(self):
+        '''
+        Checks that the workflow and the form_step are from the same company
+        '''
         workflow_sequence = WorkflowSequence(workflow=self.workflow1, form_step=self.user_form2, order=1)
         with self.assertRaises(ValidationError):
             workflow_sequence.save(current_user=self.user1) # Should raise ValidationError because the workflow and the form_step are from different companies
-
-        workflow_sequence = WorkflowSequence(workflow=self.workflow1, form_step=self.user_form1, order=1)
-        with self.assertRaises(ValidationError):
-            workflow_sequence.save(current_user=self.user2) # Should raise ValidationError because the user is not on the same company as the workflow
+    
             
